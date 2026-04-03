a01ab37b2c9fc20289609c088f5e07ab7e78a456
luisvalle7 <luisluisxv64@gmail.com>
2026-04-03 11:53:35 -0400

    remove hash location for clean URLs

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, withHashLocation } from '@angular/router'; // <-- Importa withHashLocation
+import { provideRouter} from '@angular/router'; 
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { routes } from './app.routes';
@@ -7,7 +7,7 @@ import { routes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes, withHashLocation()), // <-- Agrega withHashLocation aquí
+    provideRouter(routes), 
     provideClientHydration(withEventReplay()),
     provideHttpClient(withFetch())
   ]
